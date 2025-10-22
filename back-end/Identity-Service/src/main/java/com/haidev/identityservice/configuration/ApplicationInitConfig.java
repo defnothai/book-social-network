041cf14e9d625ca1f48a36321451899ed5ceaa14
HaiDev <honghaiit257@gmail.com>
2025-10-22 09:10:51 +0700

    config(security): init security between services

--- back-end/Identity-Service/src/main/java/com/haidev/identityservice/configuration/ApplicationInitConfig.java
+++ back-end/Identity-Service/src/main/java/com/haidev/identityservice/configuration/ApplicationInitConfig.java
@@ -2,6 +2,10 @@ package com.haidev.identityservice.configuration;
 
 import java.util.HashSet;
 
+import com.haidev.identityservice.entity.Role;
+import com.haidev.identityservice.exception.AppException;
+import com.haidev.identityservice.exception.ErrorCode;
+import com.haidev.identityservice.repository.RoleRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.haidev.identityservice.entity.User;
-import com.haidev.identityservice.enums.Role;
 import com.haidev.identityservice.repository.UserRepository;
 
 import lombok.AccessLevel;
@@ -24,22 +27,28 @@ import lombok.extern.slf4j.Slf4j;
 public class ApplicationInitConfig {
 
     PasswordEncoder passwordEncoder;
+    RoleRepository roleRepository;
 
     @Bean
     @ConditionalOnProperty(
             prefix = "spring",
             value = "datasource.driverClassName",
-            havingValue = "com.mysql.cj.jdbc.Driver")
+            havingValue = "com.mysql.cj.jdbc.Driver"
+    )
     ApplicationRunner applicationRunner(UserRepository userRepository) {
         log.info("Application runner started");
         return args -> {
             if (!userRepository.existsByUsername("admin")) {
-                HashSet<String> roles = new HashSet<>();
-                roles.add(Role.ADMIN.name());
+                HashSet<Role> roles = new HashSet<>();
+                roles.add(roleRepository.findById(
+                        com.haidev.identityservice.enums.Role.ADMIN.name())
+                        .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_EXISTED)
+                        )
+                );
                 User user = User.builder()
                         .username("admin")
                         .password(passwordEncoder.encode("admin"))
-                        //                        .roles(roles)
+                        .roles(roles)
                         .build();
                 userRepository.save(user);
                 log.warn("Admin user created");
--- back-end/api-gateway/src/main/java/com/haidev/apigateway/configuration/WebClientConfiguration.java
+++ back-end/api-gateway/src/main/java/com/haidev/apigateway/configuration/WebClientConfiguration.java
@@ -6,22 +6,45 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
+/**
+    Cấu hình webclient và HttpServiceProxyFactory trong spring boot 6 để có thể gọi API
+    giữa các microservice (ở đây là từ api-gateway đến identity service) mà không dùng open feign.
+    + WebClient là HTTP client thực tế mà Spring sử dụng để gửi request đi giữa các service
+        (ở đây là từ api-gateway đến identity service)
+*/
 @Configuration
 public class WebClientConfiguration {
 
     @Bean
     WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8080/identity") // Địa chỉ của Identity Service
+                .baseUrl("http://localhost:8080/identity") // định nghĩa url gốc identity service mà gateway sẽ gọi đến
                 .build();
     }
 
     @Bean
     IdentityClient identityClient(WebClient webClient) {
+        // tạo ra một factory có thể sinh ra proxy (client runtime)
+        // cho các interface được annotate bằng @PostExchange, @GetExchange, ...
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
-                .builderFor(WebClientAdapter.create(webClient))
+                .builderFor(WebClientAdapter.create(webClient)) // chuyển webclient thành adapter để httpServiceProxyFactory sử dụng
                 .build();
+
+        // từ interface IdentityClient, Spring sinh ra một proxy class tự động thực thi.
         return httpServiceProxyFactory.createClient(IdentityClient.class);
     }
 }
+/**
+ * LUỒNG HOẠT ĐỘNG:
+    1. webClient() -> tạo một instance WebClient đã gắn baseUrl
+    2. identity(webClient) -> nhận WebClient vừa tạo -> tạo ra proxy object cho IdentityClient
+    3. khi đó, spring context có 2 bean: webclient, IdentityClient   (proxy)
+    4. khi sử dụng IdentityClient trong service thì tức là đang dùng IdentityClient (proxy) bên trên
+    5. khi chạy identityClient.introspect(request), proxy intercept call introspect(request)
+    6. proxy đọc metadata từ @PostExchange
+    7. proxy sử dụng webclient mà truyền lúc tạo factory
+    8. webclient build ra một request mà url ghép với baseUrl đã định nghĩa rồi gửi request với body
+    9. Identity Service nhận được, xử lý và trả về json
+    10. Webclient nhận response, parse thành ApiResponse<IntrospectResponse>
+    11. Do introspect() đã định nghĩa interface trả về Mono<> nên dữ liệu được wrapped trong một reactive stream
+ */
