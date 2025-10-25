396d57151c1acb328eec6af26bb1dfc41f09cef5
HaiDev <honghaiit257@gmail.com>
2025-10-26 00:47:04 +0700

    config kafka

--- back-end/notification-service/src/main/java/com/haidev/notificationservice/controller/EmailController.java
+++ back-end/notification-service/src/main/java/com/haidev/notificationservice/controller/EmailController.java
@@ -7,11 +7,14 @@ import com.haidev.notificationservice.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -24,6 +27,11 @@ public class EmailController {
         return ApiResponse.<EmailResponse>builder()
                 .result(emailService.sendEmail(request))
                 .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message) {
+        log.info("Received message from topic 'onboard-successful': {}", message);
     }
 
 }
