bf2e49a69d4d7c8271c9a884dbff8bdcfd2b9710
HaiDev <honghaiit257@gmail.com>
2025-10-26 15:34:42 +0700

    config: add cors at gateway

--- back-end/api-gateway/src/main/java/com/haidev/apigateway/configuration/WebClientConfiguration.java
+++ back-end/api-gateway/src/main/java/com/haidev/apigateway/configuration/WebClientConfiguration.java
@@ -3,9 +3,15 @@ package com.haidev.apigateway.configuration;
 import com.haidev.apigateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
+
 /**
     Cấu hình webclient và HttpServiceProxyFactory trong spring boot 6 để có thể gọi API
     giữa các microservice (ở đây là từ api-gateway đến identity service) mà không dùng open feign.
@@ -20,6 +26,19 @@ public class WebClientConfiguration {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity") // định nghĩa url gốc identity service mà gateway sẽ gọi đến
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 
     @Bean
